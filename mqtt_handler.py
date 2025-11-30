45756f0b3156c5434325dc7c4fa803e00133195f
jaronmcd <108740950+jaronmcd@users.noreply.github.com>
2025-11-30 09:38:20 -0600

    Update mqtt_handler.py set "manufacturer" to "rtl-haos"

--- mqtt_handler.py
+++ mqtt_handler.py
@@ -101,7 +101,7 @@ class HomeNodeMQTT:
                 "unique_id": unique_id,
                 "device": {
                     "identifiers": [f"rtl433_{device_model}_{unique_id.split('_')[0]}"],
-                    "manufacturer": "West End Software",
+                    "manufacturer": "rtl-haos",
                     "model": device_model,
                     "name": device_name 
                 },
